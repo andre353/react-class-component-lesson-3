32a1ef1f0b56ded43f480905cd8ae7cf2ff6e34e
eva393 <alen4.andre3v4@yandex.com>
2022-07-15 10:46:32 +0300

    Initial commit

---
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from './ClassComponent.module.css';
+
+export class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 5,
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.number);
+  }
+
+  render() {
+    return (
+      <div className={style.game}>
+        <p className={style.result}>{this.state.number}</p>
+        <form className={style.form} onSubmit={this.handleSubmit}>
+          <label className={style.label} htmlFor='user_number'>
+            Угадай число
+          </label>
+          <input className={style.input} type='number' id='user_number' />
+          <button className={style.btn}>Угадать</button>
+        </form>
+      </div>
+    );
+  }
+}
